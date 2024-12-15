5834abb62a1c2a761a52a1f29cd1c4a4edeb1d80
Burak Köse <burakkose011@icloud.com>
2024-12-15 18:18:14 +0300

    middleware olusturuldu

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 const notRoute = require("./routes/notRoute");
 
-const db = require("./db");
-
 const app = express();
 app.use(cors());
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`${process.env.PORT} çalışıyor...`);
